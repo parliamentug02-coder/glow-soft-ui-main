938943298789d09cc74695f50b4c2cc5bb70a0fc
[dyad] <olekseikolovanov@gmail.com>
2025-09-13 21:02:45 +0300

    [dyad] Виправлено функціональність адмін-панелі та покращено дизайн інтерфейсу - wrote 4 file(s), executed 1 SQL queries

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
 			}
 		},
 		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'], // Додаємо Inter до стеку шрифтів sans-serif
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
